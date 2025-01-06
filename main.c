4017265aeb125da4b9cbd7b4b4dc01d353565f30
MOUACHIQ-ART <abdelkarim.mouachiq@ensea.fr>
2025-01-06 08:34:49 +0100

    Update main.c

--- main.c
+++ main.c
@@ -6,12 +6,6 @@
 #include <netdb.h>
 #include <sys/socket.h>
 
-#define USAGE_MSG "Usage: %s <host> <file>\n"
-#define ADDRESS_OBTAINED_MSG "Address obtained for host.\n"
-#define SOCKET_CREATED_MSG "Socket created successfully.\n"
-#define SOCKET_ERROR_MSG "Error creating socket.\n"
-
-
 int main(int argc, char *argv[]) {
     if (argc != 3) {
         fprintf(stderr, "Usage: %s host file\n", argv[0]);
@@ -21,18 +15,25 @@ int main(int argc, char *argv[]) {
     char *file = argv[2];
     struct addrinfo hints, *res;
     memset(&hints, 0, sizeof(hints));
-    hints.ai_family = AF_INET;
-    hints.ai_socktype = SOCK_DGRAM;
-    hints.ai_protocol = IPPROTO_UDP;
+    hints.ai_family = AF_INET;       // IPv4
+    hints.ai_socktype = SOCK_DGRAM;  // UDP
+    hints.ai_protocol = IPPROTO_UDP; // Protocole UDP
 
+    //Call to getaddrinfo
     int status = getaddrinfo(host, "1069", &hints, &res);
     if (status != 0) {
         fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
         return 2;
     }
-    
+     // Display the resolved IP address
+    if (res != NULL) {
+        char ip_str[INET_ADDRSTRLEN];
+        struct sockaddr_in *ipv4 = (struct sockaddr_in *)res->ai_addr;
+        inet_ntop(AF_INET, &(ipv4->sin_addr), ip_str, INET_ADDRSTRLEN);
+        printf(" Adresse IP du serveur : %s\n", ip_str);
+    }
+     // Socket creation 
     int sockfd = -1;
-
     if (res != NULL) {
         sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
         if (sockfd == -1) {
@@ -45,8 +46,9 @@ int main(int argc, char *argv[]) {
         fprintf(stderr, "Failed to create socket\n");
         return 3;
     }
-
-    freeaddrinfo(res);
+     freeaddrinfo(res);
     printf("Host: %s, File: %s\n", host, file);
     return 0;
 }
+
+
